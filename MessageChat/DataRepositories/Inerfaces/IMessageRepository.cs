60478e592b9cf50883670d83d17f81f38e4a9cd7
MagHErmit <->
2020-11-25 14:20:08 +1000

    Добавлен интерфейс репозитория сообщений

---
+++ MessageChat/DataRepositories/Inerfaces/IMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using MessageChat.DomainModels;
+using System.Collections.Generic;
+
+namespace MessageChat.DataRepositories
+{
+    public interface IMessageRepository
+    {
+        public MessageModel GetMessage(int id);
+
+        public List<MessageModel> GetPartMessages(int offset);
+    }
+}
---
+++ MessageChat/DataRepositories/MessageRepository.cs
@@ -0,0 +1,19 @@
+﻿using MessageChat.DomainModels;
+using System;
+using System.Collections.Generic;
+
+namespace MessageChat.DataRepositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        public MessageModel GetMessage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MessageModel> GetPartMessages(int offset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
